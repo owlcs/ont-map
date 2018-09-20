5ee57fb52645d87ef55d94e15bd14c14dcf20ac6
szuev <sss.zuev@gmail.com>
2018-09-21 00:11:16 +0300

    ont-map: minor, add capabilities to ReadOnlyGraph

--- src/main/java/ru/avicomp/map/utils/ReadOnlyGraph.java
+++ src/main/java/ru/avicomp/map/utils/ReadOnlyGraph.java
@@ -1,6 +1,8 @@
 package ru.avicomp.map.utils;
 
+import org.apache.jena.graph.Capabilities;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.sparql.graph.UnmodifiableGraph;
 
@@ -10,6 +12,53 @@ import org.apache.jena.sparql.graph.UnmodifiableGraph;
  * Created by @szuev on 21.06.2018.
  */
 public class ReadOnlyGraph extends UnmodifiableGraph {
+
+    private static final Capabilities READ_ONLY_CAPABILITIES = new Capabilities() {
+        @Override
+        public boolean sizeAccurate() {
+            return true;
+        }
+
+        @Override
+        public boolean addAllowed() {
+            return addAllowed(false);
+        }
+
+        @Override
+        public boolean addAllowed(boolean every) {
+            return false;
+        }
+
+        @Override
+        public boolean deleteAllowed() {
+            return deleteAllowed(false);
+        }
+
+        @Override
+        public boolean deleteAllowed(boolean every) {
+            return false;
+        }
+
+        @Override
+        public boolean canBeEmpty() {
+            return true;
+        }
+
+        @Override
+        public boolean iteratorRemoveAllowed() {
+            return false;
+        }
+
+        @Override
+        public boolean findContractSafe() {
+            return true;
+        }
+
+        @Override
+        public boolean handlesLiteralTyping() {
+            return true;
+        }
+    };
 
     public ReadOnlyGraph(Graph base) {
         super(base);
@@ -21,6 +70,11 @@ public class ReadOnlyGraph extends UnmodifiableGraph {
     }
 
     @Override
+    public void remove(Node s, Node p, Node o) {
+        delete(Triple.createMatch(s, p, o));
+    }
+
+    @Override
     public void delete(Triple t) {
         throw new UnsupportedOperationException("Read only graph: can't delete triple " + t);
     }
@@ -29,4 +83,10 @@ public class ReadOnlyGraph extends UnmodifiableGraph {
     public void clear() {
         throw new UnsupportedOperationException("Read only graph: can't clear");
     }
+
+    @Override
+    public Capabilities getCapabilities() {
+        return READ_ONLY_CAPABILITIES;
+    }
+
 }
